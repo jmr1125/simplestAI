f3bf51cb56dc899608c38c40faa29b80f2536447
jiangmr <jmr1125@163.com>
2023-08-06 15:05:44 +0800

    it can almost guess what i write

--- function.cpp
+++ function.cpp
@@ -1,4 +1,4 @@
-#include "train.h"
+#include "function.h"
 #include "layer.h"
 #include "main.h"
 #include "matrix.h"
@@ -10,58 +10,6 @@
 #include <omp.h>
 #endif
 #include <vector>
-void train(network &net, const VvalT &input, const VvalT &expect,
-           const valT scale, valT *progress) {
-  net.setInput(input);
-  net.getV();
-  // delta d? = delta dv v d? = 2(delta-v)*vd?
-  // modify it
-  vector<matrix> netVw;
-  vector<matrix> netVb;
-  netVw.reserve(net.layers.size());
-  netVb.reserve(net.layers.size());
-  for (int i = 0; i < net.layers.size(); ++i) {
-    netVw[i] = net.layers[i].w;
-    netVb[i] = net.layers[i].b;
-  }
-#ifdef USE_OMP
-#pragma omp parallel for
-#endif
-  valT total = [&]() -> valT {
-    valT a = 0;
-    for (size_t l = 0; l < net.layers.size(); ++l) {
-      a += net.layers[l].w.getm() + 1;
-    }
-    return net.output.size() * a;
-  }();
-  valT done = 0;
-  for (size_t i = 0; i < net.output.size(); ++i) {
-    valT v = (net.output[i] - expect[i]) * 2; //(V_i-e)^2 d? = 2(V_i-e)*V_i d ?
-    v *= scale;
-    for (size_t l = 0; l < net.layers.size(); ++l) {
-      const matrix vdb = net.getVdbi(l);
-      const valT delta_b = vdb(i, 0) * v;
-      netVb[l](i, 0) -= delta_b;
-      net.getV();
-      if (progress)
-        ++done, *progress = done / total;
-      for (int j = 0; j < net.layers[l].w.getm(); ++j) {
-        const matrix &&vdw = net.getVdWij(l, j);
-        const valT delta_w = vdw(i, 0) * v;
-        netVb[l](i, 0) -= delta_b;
-        net.getV();
-        if (progress)
-          ++done, *progress = done / total;
-        netVw[l](i, j) -= delta_w;
-      }
-    }
-  }
-  for (int i = 0; i < net.layers.size(); ++i) {
-    net.layers[i].w = netVw[i];
-    net.layers[i].b = netVb[i];
-  }
-}
-
 valT genvalT() {
   static std::random_device rd;
   valT v = rd();
@@ -106,6 +54,3 @@ valT func_softmax(VvalT x, int i) {
   }
   return a / b;
 }
-// valT defunc_softmax(VvalT x,int i,int j){}
-
-// reference https://zh.wikipedia.org/zh-cn/%E6%BF%80%E6%B4%BB%E5%87%BD%E6%95%B0
