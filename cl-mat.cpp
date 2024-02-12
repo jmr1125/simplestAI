e0bca1b5dfb3c24a76ef3f3c94c568e79a2b3f24
jiangmr <jmr1125@163.com>
2024-02-12 08:48:51 +0800

    the matrix multiplication is ok

--- cl-mat.cpp
+++ cl-mat.cpp
@@ -1,10 +1,8 @@
-
-#ifdef __APPLE__
-#include <OpenCL/cl.h>
-#else
-#include <CL/cl.hpp>
-#endif
-#include "clGetErrorString.hpp"
+#include "cl-mat.hpp"
+#include "layer.hpp"
+#include "matrix.hpp"
+#include <OpenCL/OpenCL.h>
+#include <new>
 #include <string>
 using namespace std;
 
@@ -41,7 +39,7 @@ for(unsigned int i=0;i<k;++i){
 //res+=a[k*x+i]*b[n*i+y];
 res+=a[y*k+i]*b[i*n+x];
 }
-c[y*m+x]=res;
+c[y*n+x]=res;
 }
 )";
 
@@ -49,13 +47,13 @@ c[y*m+x]=res;
   if (ret != CL_SUCCESS) {                                                     \
     printf("ERR: " #s " : %s (%d)", clGetErrorString(ret), ret);               \
   }
-cl_kernel k_mul_mat, k_mul_vec;
+cl_int ret;
+cl_kernel k_mul_mat, k_mul_vec, k_add_vec;
 
 cl_context context;
 cl_command_queue command;
 cl_program program;
 void init() {
-  cl_int ret;
   vector<cl_platform_id> platforms;
   platforms.resize(100);
   cl_uint platform_count;
@@ -89,13 +87,209 @@ void init() {
   // create kernels
 
   k_mul_mat = clCreateKernel(program, "mul_mat", &ret);
-  right("create kernel mat");
+  right("create kernel mul mat");
   k_mul_vec = clCreateKernel(program, "mul_vec", &ret);
-  right("create kernel vec");
+  right("create kernel mul vec");
+  k_mul_vec = clCreateKernel(program, "add_vec", &ret);
+  right("create kernel add vec");
 }
 void teardown() {
   clReleaseKernel(k_mul_mat);
   clReleaseProgram(program);
   clReleaseCommandQueue(command);
   clReleaseContext(context);
+}
+
+matrix mul_mat(const matrix &a, const matrix &b) {
+  if (a.getm() != b.getn()) {
+    throw dimension_error("a.m = " + to_string(a.getm()) +
+                          "and b.n = " + to_string(b.getn()));
+  }
+  auto m = a.getn(), k = a.getm(), n = b.getm();
+  cl_mem in_a, in_b, out;
+  in_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * m * k, NULL,
+                        &ret);
+  right("create buf a");
+  in_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * k * n, NULL,
+                        &ret);
+  right("create buf b");
+  out = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(valT) * m * n, NULL,
+                       &ret);
+  right("create buf out");
+  auto A = new valT[m * k];
+  auto B = new valT[k * n];
+  auto C = new valT[m * n];
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < k; ++j) {
+      A[i * k + j] = a(i, j);
+    }
+  }
+  for (int i = 0; i < k; ++i) {
+    for (int j = 0; j < n; ++j) {
+      B[i * n + j] = b(i, j);
+    }
+  }
+
+  cl_event wrtA = clCreateUserEvent(context, &ret);
+  right("create event A");
+  cl_event wrtB = clCreateUserEvent(context, &ret);
+  right("create event B");
+  cl_event computed = clCreateUserEvent(context, &ret);
+  right("create event done");
+  ret = clEnqueueWriteBuffer(command, in_a, CL_FALSE, 0, sizeof(valT) * m * k,
+                             A, 0, NULL, &wrtA);
+  right("write a");
+  ret = clEnqueueWriteBuffer(command, in_b, CL_FALSE, 0, sizeof(valT) * k * n,
+                             B, 0, NULL, &wrtB);
+  right("write b");
+  ret = clSetKernelArg(k_mul_mat, 0, sizeof(unsigned int), &m);
+  right("set arg 0");
+  ret = clSetKernelArg(k_mul_mat, 1, sizeof(unsigned int), &n);
+  right("set arg 1");
+  ret = clSetKernelArg(k_mul_mat, 2, sizeof(unsigned int), &k);
+  right("set arg 2");
+  ret = clSetKernelArg(k_mul_mat, 3, sizeof(cl_mem), &in_a);
+  right("set arg 3");
+  ret = clSetKernelArg(k_mul_mat, 4, sizeof(cl_mem), &in_b);
+  right("set arg 4");
+  ret = clSetKernelArg(k_mul_mat, 5, sizeof(cl_mem), &out);
+  right("set arg 5");
+  cl_event waitlist[] = {wrtA, wrtB};
+  size_t global[] = {n, m};
+  ret = clEnqueueNDRangeKernel(command, k_mul_mat, 2, NULL, global, NULL, 2,
+                               waitlist, &computed);
+  right("run");
+  waitlist[0] = computed;
+  waitlist[1] = NULL;
+  ret = clEnqueueReadBuffer(command, out, CL_TRUE, 0, sizeof(valT) * m * n, C,
+                            1, waitlist, NULL);
+  right("read buffer");
+  matrix res;
+  res.setn(m);
+  res.setm(n);
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      res(i, j) = C[i * n + j];
+    }
+  }
+  delete[] A;
+  delete[] B;
+  delete[] C;
+  clReleaseMemObject(in_a);
+  clReleaseMemObject(in_b);
+  clReleaseMemObject(out);
+  return res;
+}
+
+VvalT mul_vec(const VvalT &a, const VvalT &b) {
+  if (a.size() != b.size()) {
+    throw dimension_error("a.m = " + to_string(a.size()) +
+                          "and b.n = " + to_string(b.size()));
+  }
+  auto n = b.size();
+  cl_mem in_a, in_b, out;
+  in_a =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * n, NULL, &ret);
+  right("create buf a");
+  in_b =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * n, NULL, &ret);
+  right("create buf b");
+  out =
+      clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(valT) * n, NULL, &ret);
+  right("create buf out");
+
+  cl_event wrtA = clCreateUserEvent(context, &ret);
+  right("create event A");
+  cl_event wrtB = clCreateUserEvent(context, &ret);
+  right("create event B");
+  cl_event computed = clCreateUserEvent(context, &ret);
+  right("create event done");
+  ret = clEnqueueWriteBuffer(command, in_a, CL_FALSE, 0, sizeof(valT) * n,
+                             a.data(), 0, NULL, &wrtA);
+  right("write a");
+  ret = clEnqueueWriteBuffer(command, in_b, CL_FALSE, 0, sizeof(valT) * n,
+                             b.data(), 0, NULL, &wrtB);
+  right("write b");
+
+  ret = clSetKernelArg(k_mul_mat, 0, sizeof(unsigned int), &n);
+  right("set arg 0");
+  ret = clSetKernelArg(k_mul_mat, 1, sizeof(cl_mem), &in_a);
+  right("set arg 1");
+  ret = clSetKernelArg(k_mul_mat, 2, sizeof(cl_mem), &in_b);
+  right("set arg 2");
+  ret = clSetKernelArg(k_mul_mat, 3, sizeof(cl_mem), &out);
+  right("set arg 3");
+  cl_event waitlist[] = {wrtA, wrtB};
+  size_t global[] = {n};
+  ret = clEnqueueNDRangeKernel(command, k_mul_vec, 1, NULL, global, NULL, 2,
+                               waitlist, &computed);
+  right("run");
+  waitlist[0] = computed;
+  waitlist[1] = NULL;
+  VvalT res;
+  res.resize(n);
+  ret = clEnqueueReadBuffer(command, out, CL_TRUE, 0, sizeof(valT) * n,
+                            res.data(), 1, waitlist, NULL);
+  right("read buffer");
+  clReleaseMemObject(in_a);
+  clReleaseMemObject(in_b);
+  clReleaseMemObject(out);
+  return res;
+}
+
+VvalT add_vec(const VvalT &a, const VvalT &b) {
+  if (a.size() != b.size()) {
+    throw dimension_error("a.m = " + to_string(a.size()) +
+                          "and b.n = " + to_string(b.size()));
+  }
+  auto n = b.size();
+  cl_mem in_a, in_b, out;
+  in_a =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * n, NULL, &ret);
+  right("create buf a");
+  in_b =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(valT) * n, NULL, &ret);
+  right("create buf b");
+  out =
+      clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(valT) * n, NULL, &ret);
+  right("create buf out");
+
+  cl_event wrtA = clCreateUserEvent(context, &ret);
+  right("create event A");
+  cl_event wrtB = clCreateUserEvent(context, &ret);
+  right("create event B");
+  cl_event computed = clCreateUserEvent(context, &ret);
+  right("create event done");
+  ret = clEnqueueWriteBuffer(command, in_a, CL_FALSE, 0, sizeof(valT) * n,
+                             a.data(), 0, NULL, &wrtA);
+  right("write a");
+  ret = clEnqueueWriteBuffer(command, in_b, CL_FALSE, 0, sizeof(valT) * n,
+                             b.data(), 0, NULL, &wrtB);
+  right("write b");
+
+  ret = clSetKernelArg(k_mul_mat, 0, sizeof(unsigned int), &n);
+  right("set arg 0");
+  ret = clSetKernelArg(k_mul_mat, 1, sizeof(cl_mem), &in_a);
+  right("set arg 1");
+  ret = clSetKernelArg(k_mul_mat, 2, sizeof(cl_mem), &in_b);
+  right("set arg 2");
+  ret = clSetKernelArg(k_mul_mat, 3, sizeof(cl_mem), &out);
+  right("set arg 3");
+  cl_event waitlist[] = {wrtA, wrtB};
+  size_t global[] = {n};
+  ret = clEnqueueNDRangeKernel(command, k_add_vec, 1, NULL, global, NULL, 2,
+                               waitlist, &computed);
+  right("run");
+  waitlist[0] = computed;
+  waitlist[1] = NULL;
+  VvalT res;
+  res.resize(n);
+  ret = clEnqueueReadBuffer(command, out, CL_TRUE, 0, sizeof(valT) * n,
+                            res.data(), 1, waitlist, NULL);
+  right("read buffer");
+  clReleaseMemObject(in_a);
+  clReleaseMemObject(in_b);
+  clReleaseMemObject(out);
+  return res;
 }
